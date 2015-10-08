ece11e7c9b67cecd65d4151f1cc5e14b5d3c0a70
Daniel Windham <dewindham@gmail.com>
2015-10-07 23:38:02 -0400

    root: wrap in a closure; other minor style tweaks

--- js/src/root.js
+++ js/src/root.js
@@ -1,6 +1,14 @@
-if (typeof(OO) === "undefined") {
-  OO = {};
-};
+(function() {
+
+// establish the root object: `window` in browser, `global` on server
+var root = this;
+
+// save the previous value of the `OO` variable
+var previousOO = root.OO;
+
+// top level namespace
+var OO = root.OO = {};
+OO.previousOO = previousOO;
 
 // tell the grammar about OO
 O.OO = OO;
@@ -13,3 +21,5 @@ OO.reset = function() {
   this.core.reset();
   this.classes.setupNativeClasses();
 };
+
+})();
