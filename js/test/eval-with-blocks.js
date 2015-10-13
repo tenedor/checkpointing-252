e4d9b382e413e4f2e9a6fddc6336228b04f16bdb
Duligur Ibeling <duligur@gmail.com>
2015-10-13 00:30:50 -0400

    tests for loop and if

--- js/test/eval-with-blocks.js
+++ js/test/eval-with-blocks.js
@@ -1,17 +1,27 @@
 tests(O,
   {
-    name: 'thenElse (1/2)',
-    code: 'def True then tb else fb = tb.call();\n' +
-          'def False then tb else fb = fb.call();\n' +
-          '1 > 2 then {111} else {222}',
+    name: 'ifThenElse (1/2)',
+    code: '(1 > 2).ifThenElse({111}, {222})',
     expected: 222
   },
   {
-    name: 'thenElse (2/2)',
-    code: 'def True then tb else fb = tb.call();\n' +
-          'def False then tb else fb = fb.call();\n' +
-          '1 < 2 then {111} else {222}',
+    name: 'ifThenElse (2/2)',
+    code: '(1 < 2).ifThenElse({111}, {222})',
     expected: 111
+  },
+  {
+    name: 'loop (1/2)',
+    code: 'var f = 0;\n' +
+          '10.loop({f = f+2;});\n' +
+          'f;',
+    expected: 20
+  },
+  {
+    name: 'loop (2/2)',
+    code: 'var f = 0;\n' +
+          '0.loop({f = f+2;});\n' +
+          'f;',
+    expected: 0
   },
   {
     name: 'non-local return (1/2)',
