c514d435c6e1f83548a5b379a7d74cc48d2d65cd
Eric Lu <penlume@gmail.com>
2015-12-08 07:19:58 -0500

    solver/ast: updated OOC ast

--- solver/ast.ml
+++ solver/ast.ml
@@ -1,33 +1,18 @@
 type var = string
-type pos = int      (* position is line number in source file *)
 
 type binop = 
-  Plus | Minus | Times | Div          (* +, -, *, /           *)
-| Eq | Neq | Lt | Lte | Gt | Gte      (* ==, !=, <, <=, >, >= *)
+  Plus | Eq
 
-type rexp = 
-  Int of int
-| Var of var
-| Binop of exp * binop * exp
+type exp = 
+  Int of int                          (* may in practice just be 0 or 1 *)
+| Var of var                          (* n[0-9]*(_[0-9]* )* or k... or B...[varname] *)
+| Plus of exp * exp                   (* z1+z2 *)
+| Eq of exp * exp                     (* z1=z2 *)
 | Not of exp                          (* !x *)
-| And of exp * exp                    (* x < y && y < z *)
-| Or of exp * exp                     (* x < y || x < z *)
-| Assign of var * exp                 (* x = y+42 *)
+| Half of exp                         (* x/2 *)
 
-(* every expression comes with its position *)
-and exp = rexp * pos
-
-type rstmt = 
-  Exp of exp                          (* x = 3+4; *)
-| Seq of stmt * stmt                  (* x = 2*9; y = 42; *)
-| If of exp * stmt * stmt             (* if (x == y) x = 42 else y = 43 *)
-| While of exp * stmt                 (* while (x < y) x = x + 1; *)
-| For of exp * exp * exp * stmt       (* for (x=0; x<y; x=x+1) y=y*42; *)
-| Return of exp                       (* return e; *)
-
-(* every statement comes with its position *)
-and stmt = rstmt * pos
-
-let skip : rstmt = Exp(Int 0,0)          (* simulate a skip statement *)
-
-type program = stmt
+(* OOC instance *)
+type inst = 
+  Clause of exp
+| And of inst * inst                  (* S1 & S2 *)
+| Or of inst * inst                   (* S1 | S2 *)
