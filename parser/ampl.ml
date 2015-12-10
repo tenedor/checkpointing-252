f7be337aa2765cbf49ddb2b06ba81ae315bdb29c
Eric Lu <penlume@gmail.com>
2015-12-10 01:13:13 -0500

    solver: adapted ampl converter to new AST

--- parser/ampl.ml
+++ parser/ampl.ml
@@ -3,49 +3,34 @@ open Ast
 
 exception Unimplemented
 
-module VarSet = Set.Make(struct type t = var let compare = compare end)
-type var_set = VarSet.t
-let empty_set = VarSet.empty
-let add_var vs s = VarSet.add s vs
-let remove_var vs s = 
-  if VarSet.mem s vs
-  then VarSet.remove s vs
-  else vs
-let set_union v1 v2 = VarSet.union v1 v2
-let set_to_list vs = VarSet.elements vs
-
 (* inequality expressions: purely in terms of inequalities *)
-type iexp =
+type exp =
   Int of int
 | Var of var
-| Plus of iexp * iexp
-| Times of int * iexp
-| Leq of iexp * iexp
-| Geq of iexp * iexp
-| Not of iexp
-| Half of iexp
+| Plus of exp * exp
+| Times of int * exp
 
-(* new instruction type uses iexps *)
+(* new instruction type uses exps *)
 type inst =
-  Clause of iexp
+  Eq of exp * exp
+| Leq of exp * exp
+| Geq of exp * exp
+| True of var
+| False of var
 | And of inst * inst
 | Or of inst * inst
 
-let rec iexp_tostring (e : iexp) : string =
-  let plus_under_times (e : iexp) : string =
+let rec exp_tostring (e : exp) : string =
+  let plus_under_times (e : exp) : string =
     match e with
-      Plus (e1, e2) -> "(" ^ (iexp_tostring e) ^ ")"
-    | _ -> iexp_tostring e
+      Plus (e1, e2) -> "(" ^ (exp_tostring e) ^ ")"
+    | _ -> exp_tostring e
   in
   match e with
     Int i -> string_of_int i
   | Var v -> v
   | Plus (e1, e2) -> (plus_under_times e1) ^ " + " ^ (plus_under_times e2)
-  | Times (i, e2) -> (string_of_int i) ^ " * " ^ (iexp_tostring e2)
-  | Geq (e1, e2) -> (iexp_tostring e1) ^ " >= " ^ (iexp_tostring e2)
-  | Leq (e1, e2) -> (iexp_tostring e1) ^ " <= " ^ (iexp_tostring e2)
-  | Not e -> "!(" ^ (iexp_tostring e) ^ ")"
-  | Half e -> "(" ^ (iexp_tostring e) ^ ")/2"
+  | Times (i, e2) -> (string_of_int i) ^ " * " ^ (exp_tostring e2)
 
 let inst_tostring (p : inst) : string =
   let rec spaces (i : int) : string =
@@ -58,106 +43,81 @@ let inst_tostring (p : inst) : string =
     | _ -> indented margin i
   and indented (margin : int) (p : inst) =
     match p with
-      Clause e -> "(" ^ (iexp_tostring e) ^ ")"
+      Eq (e1, e2) -> (exp_tostring e1) ^ " = " ^ (exp_tostring e2)
+    | Geq (e1, e2) -> (exp_tostring e1) ^ " >= " ^ (exp_tostring e2)
+    | Leq (e1, e2) -> (exp_tostring e1) ^ " <= " ^ (exp_tostring e2)
+    | True v -> v
+    | False v -> "!" ^ v 
     | And (i1, i2) -> (or_under_and margin i1) ^ " &\n" ^ (spaces margin) ^ (or_under_and margin i2)
     | Or (i1, i2) -> (indented margin i1) ^ " | " ^ (indented margin i2)
   in (indented 0 p)
 
-(* collect binary parameters: cp *)
-let collect_params (i : inst) : var_set =
-  let rec collect_params_iexp (e : iexp) : var_set =
+type param =
+  Binary of var
+| Integer of var
+
+let param_extract p =
+  match p with
+    Binary v -> v
+  | Integer v -> v
+
+let param_compare v1 v2 = compare (param_extract v1) (param_extract v2)
+
+module ParamSet = Set.Make(struct type t = param let compare = param_compare end)
+type param_set = ParamSet.t
+let empty_set = ParamSet.empty
+let add_param ps s = ParamSet.add s ps
+let remove_param ps s = 
+  if ParamSet.mem s ps
+  then ParamSet.remove s ps
+  else ps
+let set_union p1 p2 = ParamSet.union p1 p2
+let set_to_list ps = ParamSet.elements ps
+
+(* collect parameters: cp, n, k, B *)
+let collect_params (i : inst) : param_set = 
+  let rec collect_params_exp (e : exp) : param_set =
     match e with
       Int _ -> empty_set
-    | Var v -> if (String.sub v 0 2) = "cp" then add_var empty_set v else empty_set
-    | Plus (e1, e2) | Leq (e1, e2) | Geq (e1, e2) ->
-      set_union (collect_params_iexp e1) (collect_params_iexp e2)
-    | Times (_, e) | Not e | Half e -> collect_params_iexp e
+    | Var v -> add_param empty_set (Integer v)
+    | Plus (e1, e2) -> set_union (collect_params_exp e1) (collect_params_exp e2)
+    | Times (_, e) -> collect_params_exp e
   in
-  let rec collect_params_inst (i : inst) : var_set =
+  let rec collect_params_inst (i : inst) : param_set =
     match i with
-      Clause e -> collect_params_iexp e
-    | And (i1, i2) | Or (i1, i2) -> set_union (collect_params_inst i1) (collect_params_inst i2)
+      Eq (e1, e2) | Leq (e1, e2) | Geq (e1, e2) ->
+        set_union (collect_params_exp e1) (collect_params_exp e2)
+    | True v | False v -> add_param empty_set (Binary v)
+    | And (i1, i2) | Or (i1, i2) ->
+        set_union (collect_params_inst i1) (collect_params_inst i2)
   in collect_params_inst i
 
-(* collect variables: n, k, B *)
-let collect_vars (i : inst) : var_set = 
-  let rec collect_vars_iexp (e : iexp) : var_set =
-    match e with
-      Int _ -> empty_set
-    | Var v -> let head = String.sub v 0 1 in
-      if head = "n" || head = "k" || head = "B"
-      then add_var empty_set v
-      else empty_set
-    | Plus (e1, e2) | Leq (e1, e2) | Geq (e1, e2) -> set_union (collect_vars_iexp e1) (collect_vars_iexp e2)
-    | Times (_, e) | Not e | Half e -> collect_vars_iexp e
-  in
-  let rec collect_vars_inst (i : inst) : var_set =
-    match i with
-      Clause e -> collect_vars_iexp e
-    | And (i1, i2) | Or (i1, i2) -> set_union (collect_vars_inst i1) (collect_vars_inst i2)
-  in collect_vars_inst i
-
 (* generate binary parameter declarations *)
-let decl_params (p : var_set) : string = 
-  let rec decl_param_list (pl : string list) : string =
+let decl_params (p : param_set) : string = 
+  let rec decl_param_list (pl : param list) : string =
     match pl with
-      p :: pr -> "var " ^ p ^ " binary;\n" ^ (decl_param_list pr)
+      (Binary p) :: pr -> "var " ^ p ^ " binary;\n" ^ (decl_param_list pr)
+    | (Integer p) :: pr -> "var " ^ p ^ " integer;\n" ^ (decl_param_list pr)
     | _ -> ""
   in decl_param_list (set_to_list p)
-
-(* generate variable declarations *)
-let decl_vars (v : var_set) : string =
-  let rec decl_var_list (pl : string list) : string =
-    match pl with
-      p :: pr -> "var " ^ p ^ " integer ;\n" ^ (decl_var_list pr)
-    | _ -> ""
-  in decl_var_list (set_to_list v)
 
 (* generate an objective clause with given checkpoint/query cost multipliers *)
 let minimization (cp_km : float) (q_km : float) : string = 
   "minimize prog_cost : " ^
   (string_of_float cp_km) ^ " * n2 + " ^ (string_of_float q_km) ^ " * k2 ;\n"
 
-(* contains a halving operation? *)
-let rec halves (e : iexp) : bool = 
-  match e with
-    Int i -> false
-  | Var v -> false
-  | Plus (e1, e2) | Leq (e1, e2) | Geq (e1, e2) -> (halves e1) || (halves e2)
-  | Times (_, e) | Not e -> halves e
-  | Half e -> true
-
-(* double the expression, distribute and simplify *)
-let rec double (e : iexp) : iexp = 
-  match e with
-    Int i -> Int (2 * i)
-  | Var v -> Times (2, e)
-  | Plus (e1, e2) -> Plus (double e1, double e2)
-  | Times (i, e) -> Times (2 * i, e)
-  | Leq (e1, e2) -> Leq (double e1, double e2)
-  | Geq (e1, e2) -> Geq (double e1, double e2)
-  | Not e -> double e
-  | Half e -> e
-
-exception EncounteredDoubleHalf
-exception EncounteredHalf
-
 (* convert instance to a set of constraints *)
 let constraints (i : inst) : string = 
-  let rec plus_under_times (e : iexp) : string =
+  let rec plus_under_times (e : exp) : string =
     match e with
-      Plus (_, _) -> " ( " ^ (iexp_constraint e) ^ " ) "
-    | _ -> (iexp_constraint e)
-  and iexp_constraint (e : iexp) : string =
+      Plus (_, _) -> " ( " ^ (exp_constraint e) ^ " ) "
+    | _ -> (exp_constraint e)
+  and exp_constraint (e : exp) : string =
     match e with
       Int i -> string_of_int i
     | Var v -> v
-    | Plus (e1, e2) -> (iexp_constraint e1) ^ " + " ^ (iexp_constraint e2) 
+    | Plus (e1, e2) -> (exp_constraint e1) ^ " + " ^ (exp_constraint e2) 
     | Times (i, e) -> (string_of_int i) ^ " * " ^ (plus_under_times e)
-    | Leq (e1, e2) -> " ( " ^ (iexp_constraint e1) ^ " <= " ^ (iexp_constraint e2) ^ " ) "
-    | Geq (e1, e2) -> " ( " ^ (iexp_constraint e1) ^ " >= " ^ (iexp_constraint e2) ^ " ) "
-    | Not e -> " not ( " ^ (iexp_constraint e) ^ " ) "
-    | Half e -> raise EncounteredHalf (*" ( " ^ (iexp_constraint e) ^ " )/2 "*)
   in
   let rec or_under_and (i : inst) : string =
     match i with
@@ -165,62 +125,62 @@ let constraints (i : inst) : string =
     | _ -> (inst_constraint i)
   and inst_constraint (i : inst) : string =
     match i with
-      Clause e -> (iexp_constraint e) ^ "\n"
-    | And (i1, i2) -> (or_under_and i1) ^ " and " ^ (or_under_and i2)
-    | Or (i1, i2) -> (inst_constraint i1) ^ " or " ^ (inst_constraint i2) 
+      Eq (e1, e2) -> " ( " ^ (exp_constraint e1) ^ " == " ^ (exp_constraint e2) ^ " ) "
+    | Leq (e1, e2) -> " ( " ^ (exp_constraint e1) ^ " <= " ^ (exp_constraint e2) ^ " ) "
+    | Geq (e1, e2) -> " ( " ^ (exp_constraint e1) ^ " >= " ^ (exp_constraint e2) ^ " ) "
+    | True v -> " ( " ^ v ^ " == 1 )"
+    | False v -> " ( " ^ v ^ " == 0 )"
+    | And (i1, i2) -> " ( " ^ (or_under_and i1) ^ " and " ^ (or_under_and i2) ^ " ) \n"
+    | Or (i1, i2) -> " ( " ^ (inst_constraint i1) ^ " or " ^ (inst_constraint i2) ^ " ) \n"
   in
   "subject to prog_cond : \n" ^ (inst_constraint i) ^ " ; \n"
 
-exception EncounteredEq
-
-(* rectify inequalities by doubling appropriately *)
-let rec rectify_leq (l : iexp) (r : iexp) : iexp * iexp =
-  if (halves l) && (halves r) then raise EncounteredDoubleHalf
-  else if halves l then let r, l = rectify_geq r l in (l, r)
-  else if halves r then rectify_leq (double l) (double r)
-  else (l, r)
-and rectify_geq (l : iexp) (r : iexp) : iexp * iexp =
-  if (halves l) && (halves r) then raise EncounteredDoubleHalf
-  else if halves l then let r, l = rectify_leq r l in (l, r)
-  else if halves r then rectify_geq (Plus (double l, Int 1)) (double r)
-  else (l, r)
+(* contains a halving operation? *)
+let rec halves (e : Ast.exp) : bool = 
+  match e with
+    Int i -> false
+  | Var v -> false
+  | Plus (e1, e2) -> (halves e1) || (halves e2)
+  | Half e -> true
 
 let rec to_ineq (i : Ast.inst) : inst =
-  let rec exp_to_ineq (e : Ast.exp) : iexp =
+  (* rectify inequalities by doubling appropriately *)
+  let rec rectify (e : Ast.exp) : int * exp =
     match e with
-      Ast.Int i -> Int i
-    | Ast.Var v -> Var v
-    | Ast.Plus (e1, e2) -> Plus ((exp_to_ineq e1), (exp_to_ineq e2))
-    | Ast.Eq (e1, e2) -> raise EncounteredEq
-    | Ast.Not e -> Not (exp_to_ineq e)
-    | Ast.Half e -> Half (exp_to_ineq e)
+      Ast.Int i -> (1, Int i)
+    | Ast.Var v -> (1, Var v)
+    | Ast.Plus (e1, e2) ->
+      let f1, e1 = rectify e1 in
+      let f2, e2 = rectify e2 in
+      let fe1 = if f1 != 1 then Times (f1, e1) else e1 in
+      let fe2 = if f2 != 1 then Times (f2, e2) else e2 in
+      (1, Plus (fe1, fe2)) (* can distribute down if desired *)
+    | Ast.Half e -> let f, e = rectify e in (2 * f, e)
   in
   match i with
-    Ast.Clause (Eq (e1, e2)) ->
-      let ie1 = exp_to_ineq e1 in
-      let ie2 = exp_to_ineq e2 in
-      let rl1, rl2 = rectify_leq ie1 ie2 in
-      let rg1, rg2 = rectify_geq ie1 ie2 in
-      And (Clause (Leq (rl1, rl2)), Clause (Geq (rg1, rg2)))
-  | Ast.Clause (Var v) -> to_ineq (Clause (Ast.Eq (Ast.Var v, Ast.Int 1)))
-  | Ast.Clause (Not (Var v)) -> to_ineq (Clause (Ast.Eq (Ast.Var v, Ast.Int 0)))
-  | Ast.Clause e -> Clause (exp_to_ineq e)
+    Ast.Assign (v, e) ->
+    let f, rhs = rectify e in
+    if f != 1 then
+      let fv = Times (f, Var v) in
+      And (Leq (fv, rhs),
+           Geq (Plus (fv, Int 1), rhs))
+    else
+      Eq (Var v, rhs)
+  | Ast.True v -> True v
+  | Ast.False v -> False v
   | Ast.And (i1, i2) -> And ((to_ineq i1), (to_ineq i2))
   | Ast.Or (i1, i2) -> Or ((to_ineq i1), (to_ineq i2))
 
 let ampl (i : Ast.inst) : string =
   let i = to_ineq i in
-  (* prerr_endline (inst_tostring i); (* enable for AST output *) *) 
+  (*prerr_endline (inst_tostring i); (* enable for AST output *) *)
   let par_decl = decl_params (collect_params i) in
-  let var_decl = decl_vars (collect_vars i) in
   let min = (minimization 0. 1.) in
   let subj = (constraints i) in
     "### 1. PARAMETERS ###\n" ^
     par_decl ^
-    "### 2. VARIABLES ###\n" ^
-    var_decl ^
-    "### 3. OBJECTIVE ###\n" ^
+    "### 2. OBJECTIVE ###\n" ^
     min ^
-    "### 4. CONSTRAINTS ###\n" ^
+    "### 3. CONSTRAINTS ###\n" ^
     subj
 
